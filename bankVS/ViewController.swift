c7fc87d19c37914ee02054db2c0ae7de445b4718
Vladimir Sirel <exit551@ya.ru>
2022-06-22 22:13:27 +0300

    Initial Commit

---
+++ bankVS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bankVS
+//
+//  Created by Home on 22.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
